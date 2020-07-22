90bac90a00c296f1a24bbad668e20f3a37e03af3
hycjack <772403255@qq.com>
2020-07-22 13:49:28 +0800

    add pdf excel tool

--- common/src/test/java/com/example/demo/DemoApplicationTests.java
+++ common/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
-public class DemoApplicationTests {
+import com.example.demo.utils.PdfUtil;
 
+public class DemoApplicationTests {
+    public static void main(String[] args) {
+        PdfUtil.splitPdf(7, "D:\\tt.pdf", "tt2.pdf", "D:\\");
+    }
 
 }
